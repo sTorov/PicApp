6ceb19d8e24dccee487555d014f427b5bda0be5a
sTorov <sotorov2011@yandex.ru>
2023-03-20 04:41:06 +0300

    Небольщие доработки внешнего вида

---
+++ PicApp/PicApp.Android/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using PicApp.Droid.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using static Android.Graphics.ColorSpace;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace PicApp.Droid.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            Control?.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            Control?.SetTextColor(Android.Graphics.Color.Argb(255, 200, 200, 200));
+        }
+    }
+}
